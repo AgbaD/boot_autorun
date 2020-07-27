73f409405f98eb19c3b6e69bd566136bcec02f89
BlankGodd <blankgodd33@gmail.com>
2020-07-27 14:39:23 +0100

    windows algo

--- tool.py
+++ tool.py
@@ -3,6 +3,24 @@
 
 import getpass
 from platform import system
+import os
+
+def add_for_windows(user,script_file):
+    bat = r"C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" % user
+    with open(bat+'\\'+"open.bat","w+") as bat_file:
+        bat_file.write(r'start "" %s' % script_file)
 
 if __name__ == "__main__":
-    pass
+    opsys = system()
+    if opsys == "Windows":
+        try:
+            user = getpass.getuser()
+            script_file = input("Enter file path: ")
+            add_for_windows(user,script_file)
+            print(File added)
+        except:
+            raise FileError("Could not add file to autorun!")
+    if opsys == "Linux":
+        pass
+
+        
